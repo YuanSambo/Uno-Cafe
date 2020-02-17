31cab9dc5fc1fdb02dd171c36c66de6155fff178
yuandominic <yuandominic@users.noreply.github.com>
2020-02-17 10:42:54 +0800

    Fixed menu

--- menu.php
+++ menu.php
@@ -14,33 +14,37 @@
 
         <div class="content">
             <!-- content here -->
-            <?php $results2 = $db->query("SELECT* FROM products ORDER BY categ");
+            <?php $results2 = $db->query("SELECT* FROM product_categ ORDER BY id ");
             while ($row2 = $results2->fetch_object()) : ?>
-                <div class="product-grid product-grid--flexbox" id="<?= $row2->id ?>">
+
+                <div class="product-grid product-grid--flexbox">
                     <div class="product-grid__wrapper">
-                        <!-- Product list start here -->
-                        <!-- Single product -->
-                        <form class="product-form" action="#">
-                            <div class="product-grid__product-wrapper">
-                                <div class="product-grid__product">
-                                    <div class="product-grid__img-wrapper">
-                                        <img src="img/americano.jpg" width="100%" height="100%" />
-                                    </div>
-                                    <span class="product-grid__title"><?= $row2->product_name ?></span>
-                                    <input type="hidden" name="product-title" value="<?= $row2->product_name ?>">
-                                    <span class="product-grid__price"><?= $row2->price ?></span>
-                                    <input type="hidden" name="product-price" value="<?= $row2->price ?>">
-                                    <div class="product-grid__extend-wrapper">
-                                        <div class="product-grid__extend">
-                                            <p class="product-grid__description"><?= $row2->description ?></p>
-                                            <input type="hidden" name="product-desc" value="<?= $row2->description ?>">
-                                            <input class="product-grid__btn product-grid__add-to-cart " type="submit" value="Add to Cart">
-                                            <input type="hidden" name="Add-to-Cart" value="Add to Cart">
+                        <?php $results3 = $db->query("SELECT* FROM products WHERE categ=$row2->id");
+                        while ($row3 = $results3->fetch_object()) : ?>
+                            <!-- Product list start here -->
+                            <!-- Single product -->
+                            <form class="product-form" action="#">
+                                <div class="product-grid__product-wrapper">
+                                    <div class="product-grid__product">
+                                        <div class="product-grid__img-wrapper">
+                                            <img src="img/americano.jpg" width="100%" height="100%" />
+                                        </div>
+                                        <span class="product-grid__title"><?= $row3->product_name ?></span>
+                                        <input type="hidden" name="product-title" value="<?= $row3->product_name ?>">
+                                        <span class="product-grid__price"><?= $row3->price ?></span>
+                                        <input type="hidden" name="product-price" value="<?= $row3->price ?>">
+                                        <div class="product-grid__extend-wrapper">
+                                            <div class="product-grid__extend">
+                                                <p class="product-grid__description"><?= $row3->description ?></p>
+                                                <input type="hidden" name="product-desc" value="<?= $row3->description ?>">
+                                                <input class="product-grid__btn product-grid__add-to-cart " type="submit" value="Add to Cart">
+                                                <input type="hidden" name="Add-to-Cart" value="Add to Cart">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        <?php endwhile; ?>
                     </div>
                 </div>
             <?php endwhile; ?>
